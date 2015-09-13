7cec9fe57b37825af963f8f4ff779f5dbc738c93
Sankalp Bihani <sankalpbihani.008@gmail.com>
2015-09-13 16:23:44 +0530

    basic and/or evaluation with trace done

--- self.pl
+++ self.pl
@@ -18,8 +18,22 @@ eval(Goal, [N :: (Goal is Answer) was 'derived by' (if Cond then Goal is Answer)
 	N4 is N + 4,
 	eval(Cond, T, true, N4).
 	
-%eval(Goal1 and Goal2)
+eval(Goal1 and Goal2, Trace, Answer, N) :- !,
+	eval(Goal1, Trace1, Answer1, N),
+	eval(Goal2, Trace2, Answer2, N),
+	logical_and(Answer1, Answer2, Answer),
+	append(Trace1, Trace2, Trace).
+
+eval(Goal1 or Goal2, Trace, Answer, N) :- !,
+	eval(Goal1, Trace1, Answer1, N),
+	eval(Goal2, Trace2, Answer2, N),
+	logical_or(Answer1, Answer2, Answer),
+	append(Trace1, Trace2, Trace).	
 	
+logical_and(true, true, true) :- !.
+logical_and(_, _, false).
+logical_or(false, false, false) :- !.
+logocal_or(_, _, true).
 	
 :- assertz(fact(a is true)).
 :- eval(a, [A :: B | _], _, 4), tab(A), write(B). 
