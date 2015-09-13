be7fc2f2e0c3b24bbeeeb5ee4f92432332d10c59
Sankalp Bihani <sankalpbihani.008@gmail.com>
2015-09-13 18:19:47 +0530

    basic framework for askable questions done

--- self.pl
+++ self.pl
@@ -1,42 +1,53 @@
 :- op(900, xfx, ::).
-:- op( 800, xfx, was). 
-:- op( 870, fx, if). 
-:- op( 880, xfx, then). 
-:- op( 550, xfy, or). 
-:- op( 540, xfy, and). 
-:- op( 300, fx, 'derived by'). 
-:- op( 600, xfx, from). 
-:- op( 600, xfx, by). 
-%:- op( 700, xfx, is).
-:- op( 900, fx, not). 
+:- op(800, xfx, was). 
+:- op(870, fx, if). 
+:- op(880, xfx, then). 
+:- op(550, xfy, or). 
+:- op(540, xfy, and). 
+:- op(300, fx, 'derived by'). 
+:- op(600, xfx, from). 
+:- op(600, xfx, by). 
+%:- op(700, xfx, is).
+:- op(900, fx, not). 
+
+:- op(100, xfx, has).
 
 eval(Goal, [N :: (Goal is Answer) was 'found as fact'], Answer, N) :-
-	fact(Goal is Answer).
+	fact(Goal is Answer);
+	(
+		\+ fact(Goal is Answer),
+		askable(Goal),
+		ask_user(Goal)
+	).
+	
 	
 eval(Goal, [N :: (Goal is Answer) was 'derived by' (if Cond then Goal is Answer) | T], Answer, N) :-
 	rule(if Cond then Goal is Answer),
 	N4 is N + 4,
 	eval(Cond, T, true, N4).
 	
-eval(Goal1 and Goal2, Trace, Answer, N) :- !,
+eval(Goal1 and Goal2, Trace, Answer, N) :- 
 	eval(Goal1, Trace1, Answer1, N),
 	eval(Goal2, Trace2, Answer2, N),
 	logical_and(Answer1, Answer2, Answer),
 	append(Trace1, Trace2, Trace).
 
-eval(Goal1 or Goal2, Trace, Answer, N) :- !,
+eval(Goal1 or Goal2, Trace, Answer, N) :- 
 	eval(Goal1, Trace1, Answer1, N),
 	eval(Goal2, Trace2, Answer2, N),
 	logical_or(Answer1, Answer2, Answer),
 	append(Trace1, Trace2, Trace).	
 		
-eval(not Goal, Trace, Answer, N) :- !,
-	eval(Goal, Trace, Answer1, N),
+eval(not Goal1, Trace, Answer, N) :- 
+	eval(Goal1, Trace, Answer1, N),
 	logical_not(Answer1, Answer).
 
 show_trace([]).
 show_trace([N :: Line | Remaining]) :-
 	tab(N), write(Line), nl, show_trace(Remaining).
+	
+ask_user(Goal) :-
+	write(Goal), write(' (true/false)'), read(X), assertz(fact(Goal is X)). 
 	
 logical_and(true, true, true) :- !.
 logical_and(_, _, false).
@@ -44,8 +55,11 @@ logical_or(false, false, false) :- !.
 logical_or(_, _, true).
 logical_not(true, false).
 logical_not(false, true).
+
+askable(_ has _).
 	
 :- assertz(fact(a is true)).
 :- assertz(rule(if a then b is true)).
-:- assertz(rule(if ((not a) or b) then c is true)).
+:- assertz(rule(if (not (a or b)) then c is true)).
+:- assertz(rule(if (a has b and b has a) then c is true)).
 :- eval(c, Trace, _, 0), show_trace(Trace). 
